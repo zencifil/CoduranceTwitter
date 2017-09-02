3aa97e0174554f765a756f6c73d91e93eb4c4c86
zencifil <savascilve@gmail.com>
2017-09-02 15:12:16 +0100

    Project created

---
+++ CoduranceTwitter.UI/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CoduranceTwitter.UI {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
